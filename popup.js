71faece17d11a9700546876ddb76c5c2936d754c
antonii1357 <antonii.spam@gmail.com>
2024-01-10 09:52:19 -0600

    Updated indentation and CSS

--- popup.js
+++ popup.js
@@ -1,20 +1,43 @@
-const btn = document.getElementById('btn')
-const txt = document.getElementById('editor')
-const res = document.getElementById('res')
-const a = window.console
+const btn = document.getElementById('btn'),
+    clear = document.getElementsByClassName('clear')[0],
+    txt = document.getElementById('editor'),
+    res = document.getElementById('res'),
+    originalConsole = window.console
+
+const editor = CodeMirror(document.getElementById('editor'), {
+	mode: 'javascript',
+	lineNumbers: true,
+	autoCloseBrackets: true,
+	tabSize: 4,
+	indentUnit: 4,
+})
+
+
 btn.addEventListener('click', async () => {
-    if (!txt.value) return write('Please enter a text')
+    const code = editor.getValue();
+    if (!code) return write('Please enter a text');
     try {
         console = {
             log: (msg) => msg
-        }
-        let ev = await eval(txt.value)
-        write(ev, true)
+        };
+        let ev = await eval(code);
+        write(ev);
     } catch (er) {
-        write(er)
+        write(er);
     }
-})
+});
+clear.addEventListener('click', () => (res.innerText = ''));
 
-function write(a, b) {
-    res.innerHTML = (b && typeof a == 'object') ? JSON.stringify(a, null, 4) : String(a)
+function write(a) {
+    res.innerText = a instanceof Error ? String(a) : JSON.stringify(a, censor(a), 4);
+}
+
+function censor(censor) {
+    let i = 0;
+    return function (key, value) {
+        if (i !== 0 && typeof censor === 'object' && typeof value == 'object' && censor == value) return '[Circular]';
+        if (i >= 29) return '[Unknown]';
+        ++i;
+        return value;
+    }
 }
